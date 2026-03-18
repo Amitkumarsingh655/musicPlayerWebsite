8d530807ebe85e354abd82735575a687adca4678
Amit Singh <aksingh@mplgaming.com>
2026-03-18 14:03:04 +0530

    deployment changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/musicPlayerWebsite/', // Update this if your GitHub repo name is different
   plugins: [react()],
 })
